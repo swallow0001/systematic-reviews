30c373e308501c0e58d0ac159badc998491f5c9e
Jonathan de Bruin <jonathandebruinhome@gmail.com>
2018-08-31 15:17:03 +0200

    PEP8, YAPF

--- hpc/sr_lstm.py
+++ hpc/sr_lstm.py
@@ -15,113 +15,114 @@
 
 import os
 import argparse
-import numpy as np
 
 from utils import *
 from model.textmanager import TextManager
 from model.embedding import Embedding_Layer
 from model.lstm import LSTM_Model
-import pickle
 import json
 from datetime import datetime
-
-
 
 # parse the arguments
 parser = argparse.ArgumentParser(description='Systematic Review options')
 parser.add_argument("-T", default=1, type=int, help='Task number.')
-parser.add_argument("-training_size", default=50, type=int, help='Size of training dataset')
-parser.add_argument("-allowed_FN", default=1, type=int, help='Number of allowed False Negative cases')
-parser.add_argument("-init_included_papers", default=10, type=int, help='Initial number of included papers')
-parser.add_argument("-dataset", default='ptsd', type=str, help='Name of dataset')
+parser.add_argument(
+    "-training_size", default=50, type=int, help='Size of training dataset')
+parser.add_argument(
+    "-allowed_FN",
+    default=1,
+    type=int,
+    help='Number of allowed False Negative cases')
+parser.add_argument(
+    "-init_included_papers",
+    default=10,
+    type=int,
+    help='Initial number of included papers')
+parser.add_argument(
+    "-dataset", default='ptsd', type=str, help='Name of dataset')
 sr_args = parser.parse_args()
 print(sr_args)
 
-
-pickle_file_name = sr_args.dataset+'_pickle.pickle'
-PICKLE_File = path.join(PICKLE_PATH,pickle_file_name)
-
+pickle_file_name = sr_args.dataset + '_pickle.pickle'
+PICKLE_File = path.join(PICKLE_PATH, pickle_file_name)
 
 start = datetime.now()
 # Load dataset
 # get the texts and their corresponding labels
 if sr_args.dataset == 'ptsd':
     texts, labels = load_ptsd_data()
-else: 
+else:
     texts, labels = load_drug_data(sr_args.dataset)
 
 textmanager = TextManager()
-data, labels,word_index = textmanager.sequence_maker(texts,labels)
+data, labels, word_index = textmanager.sequence_maker(texts, labels)
 max_num_words = textmanager.max_num_words
 max_sequence_length = textmanager.max_sequence_length
 
 #Split dataset to train and test
-x_train, x_val, y_train, y_val = split_data(data, labels,sr_args.training_size, sr_args.init_included_papers, 2017+sr_args.T)
+x_train, x_val, y_train, y_val = split_data(
+    data, labels, sr_args.training_size, sr_args.init_included_papers,
+    2017 + sr_args.T)
 print("x_train shape:", x_train.shape, ", x_val shape:", x_val.shape)
 print("y_train shape:", y_train.shape, ", y_val shape:", y_val.shape)
-print("included in train",(y_train[:,1]==1).sum())
-print("included in test",(y_val[:,1]==1).sum())
+print("included in train", (y_train[:, 1] == 1).sum())
+print("included in test", (y_val[:, 1] == 1).sum())
 
-
-runtime = datetime.now()-start
+runtime = datetime.now() - start
 print('loading dataset takes', runtime.total_seconds())
 
-
 if os.path.isfile(PICKLE_File):
-   start = datetime.now() 
-   embedding_layer = load_pickle(PICKLE_File)
-   runtime = datetime.now()-start
-   print('loading pickle takes', runtime.total_seconds())
+    start = datetime.now()
+    embedding_layer = load_pickle(PICKLE_File)
+    runtime = datetime.now() - start
+    print('loading pickle takes', runtime.total_seconds())
 
-   
-else:     
-    start = datetime.now() 
+else:
+    start = datetime.now()
 
     # make an embedding layer
-    embedding = Embedding_Layer(word_index,max_num_words,max_sequence_length)
+    embedding = Embedding_Layer(word_index, max_num_words, max_sequence_length)
     embedding.load_word2vec_data(WORD2VEC_PATH)
     embedding_layer = embedding.build_embedding()
-    dump_pickle(embedding_layer,PICKLE_File)
-    runtime = datetime.now()-start
+    dump_pickle(embedding_layer, PICKLE_File)
+    runtime = datetime.now() - start
     print('loading w2vec takes', runtime.total_seconds())
-
 
 # make a lstm model
 deep_model = LSTM_Model
 args_model = {
-    'backwards':False,
-    'dropout':0.3,
-    'optimizer':'rmsprop',
-    'max_sequence_length':max_sequence_length,
-    'embedding_layer':embedding_layer
+    'backwards': False,
+    'dropout': 0.3,
+    'optimizer': 'rmsprop',
+    'max_sequence_length': max_sequence_length,
+    'embedding_layer': embedding_layer
 }
 start = datetime.now()
 model = deep_model(**args_model)
 model.train(x_train, y_train, x_val, y_val)
-tn, fp, fn, tp, pred = model.score(x_val, y_val,sr_args.allowed_FN)
-runtime = datetime.now()-start
+tn, fp, fn, tp, pred = model.score(x_val, y_val, sr_args.allowed_FN)
+runtime = datetime.now() - start
 print('training model takes', runtime.total_seconds())
 
-print( 'best_tn' ,tn,
-        'best_fp' ,fp,
-        'best_fn' ,fn, 
-        'best_tp' ,tp)
-		
-seed_val = 2017+int(sr_args.T)
-lstm_scores = {  'T': sr_args.T,
-				 'tn': tn,
-				 'fp': fp,
-				 'fn': fn,
-				 'tp': tp,
-                 'allowed_FN': sr_args.allowed_FN,
-                 'init_included_papers': sr_args.init_included_papers,
-				 'dataset': sr_args.dataset,
-				 'seed': seed_val,
-				 'training_size':sr_args.training_size,
-                 'pred': [pred]}
+print('best_tn', tn, 'best_fp', fp, 'best_fn', fn, 'best_tp', tp)
+
+seed_val = 2017 + int(sr_args.T)
+lstm_scores = {
+    'T': sr_args.T,
+    'tn': tn,
+    'fp': fp,
+    'fn': fn,
+    'tp': tp,
+    'allowed_FN': sr_args.allowed_FN,
+    'init_included_papers': sr_args.init_included_papers,
+    'dataset': sr_args.dataset,
+    'seed': seed_val,
+    'training_size': sr_args.training_size,
+    'pred': [pred]
+}
 
 # save the result to a file
 if not os.path.exists('output'): os.makedirs('output')
 export_path = os.path.join('output', 'sr_lstm{}.json'.format(sr_args.T))
-with open(export_path,'w') as outfile:
-    json.dump(lstm_scores,outfile)
+with open(export_path, 'w') as outfile:
+    json.dump(lstm_scores, outfile)
--- hpc/utils.py
+++ hpc/utils.py
@@ -5,14 +5,14 @@ import pandas as pd
 import numpy as np
 import pickle
 
-
-
-DATA_PATH = path.join("..","data")
-PTSD_PATH = path.join(DATA_PATH, "ptsd_review", "csv", "schoot-lgmm-ptsd-traindata.csv")
+DATA_PATH = path.join("..", "data")
+PTSD_PATH = path.join(DATA_PATH, "ptsd_review", "csv",
+                      "schoot-lgmm-ptsd-traindata.csv")
 DRUG_DIR = path.join(DATA_PATH, "drug_class_review")
-WORD2VEC_PATH = path.join("..","word2vec", "wiki.en.vec")
-PICKLE_PATH = path.join(DATA_PATH,"pickle")
-PARAMETER_PATH = path.join(DATA_PATH,"parameters.txt")
+WORD2VEC_PATH = path.join("..", "word2vec", "wiki.en.vec")
+PICKLE_PATH = path.join(DATA_PATH, "pickle")
+PARAMETER_PATH = path.join(DATA_PATH, "parameters.txt")
+
 
 def load_ptsd_data():
     """Load ptsd papers and their labels.
@@ -24,7 +24,7 @@ def load_ptsd_data():
 
     """
 
-    print('PTSD_PATH:',PTSD_PATH)
+    print('PTSD_PATH:', PTSD_PATH)
     # read the data of the file location given as argument to this function
     df = pd.read_csv(PTSD_PATH)
 
@@ -59,7 +59,8 @@ def load_drug_data(name):
     labels = (df["label2"] == "I").astype(int)
 
     print("number of positive labels: {}".format(labels.sum()))
-    print("relative number of positive labels: {}".format(labels.sum()/labels.shape[0]))
+    print("relative number of positive labels: {}".format(
+        labels.sum() / labels.shape[0]))
 
     return texts.values, labels.values
 
@@ -69,84 +70,87 @@ def list_drug_datasets():
 
     return [dataset[:-4] for dataset in listdir(DRUG_DIR)]
 
+
 def split_data(data, labels, train_size, init_positives, seed):
     """split dataset to train and test datasets
     """
     if train_size >= len(data):
         print('invalid train size')
         return
-        
-        
-    validation_split = 1 - (train_size/len(data))
-    
+
+    validation_split = 1 - (train_size / len(data))
+
     x_train, x_val, y_train, y_val = train_test_split(
         data,
         labels,
         test_size=validation_split,
         random_state=seed,
-        stratify=labels
-    )
-    
-    
+        stratify=labels)
+
     # add added_positives positive paper to training dataset
-    if init_positives>0 :
-        ##number of included papers in train dataset
-        incl = len(np.where(y_train[:,1]==1)[0])
-        
-        ##number of included papers should be added to train dataset
+    if init_positives > 0:
+        # number of included papers in train dataset
+        incl = len(np.where(y_train[:, 1] == 1)[0])
+
+        # number of included papers should be added to train dataset
         to_add = init_positives - incl
-        
-        ## index of all included papers in test dataset 
-        positive_indx = np.where(y_val[:,1]==1)[0]
-        
-        if (to_add >0) and (len(positive_indx) >= to_add):    
-            
+
+        # index of all included papers in test dataset
+        positive_indx = np.where(y_val[:, 1] == 1)[0]
+
+        if (to_add > 0) and (len(positive_indx) >= to_add):
+
             np.random.seed(seed)
-            to_add_indx = np.random.choice(positive_indx, to_add, replace=False)
-            
-            y_train = np.vstack((y_train,y_val[to_add_indx]))
+            to_add_indx = np.random.choice(
+                positive_indx, to_add, replace=False)
+
+            y_train = np.vstack((y_train, y_val[to_add_indx]))
             x_train = np.vstack((x_train, x_val[to_add_indx]))
-            
-            x_val = np.delete(x_val, to_add_indx,0)
-            y_val = np.delete(y_val, to_add_indx,0)
+
+            x_val = np.delete(x_val, to_add_indx, 0)
+            y_val = np.delete(y_val, to_add_indx, 0)
 
     return (x_train, x_val, y_train, y_val)
 
-def dump_pickle(obj,file_name):
+
+def dump_pickle(obj, file_name):
     # open a file, where you want to store the data
     file_pickle = open(file_name, 'wb')
     # dump information to that file
     pickle.dump(obj, file_pickle)
-    
+
     # close the file
     file_pickle.close()
 
-def load_pickle(file_name):    
+
+def load_pickle(file_name):
     # open a file, where you want to store the data
     file_pickle = open(file_name, 'rb')
-    
+
     # dump information to that file
     obj = pickle.load(file_pickle)
-    
+
     # close the file
     file_pickle.close()
     return obj
 
-def save_parameters(names,parameters):
-#     with open(PARAMETER_PATH,"w") as outputfile:
-#         # writer = csv.writer(outputfile,lineterminator='\n')
-#         # writer.writerow(names)
-#         # for i, parm in enumerate(parameters):
-#         #       writer.writerow([parm[0], parm[1], parm[2], parm[3]])
-# 
-#         outputfile.write(','.join(names)+'\n')
-#         for i, param in enumerate(parameters):
-#               row = ','.join(str(x) for x in param)+'\n'
-#               outputfile.write(row)
-              
+
+def save_parameters(names, parameters):
+    #     with open(PARAMETER_PATH,"w") as outputfile:
+    #         # writer = csv.writer(outputfile,lineterminator='\n')
+    #         # writer.writerow(names)
+    #         # for i, parm in enumerate(parameters):
+    #         #       writer.writerow([parm[0], parm[1], parm[2], parm[3]])
+    #
+    #         outputfile.write(','.join(names)+'\n')
+    #         for i, param in enumerate(parameters):
+    #               row = ','.join(str(x) for x in param)+'\n'
+    #               outputfile.write(row)
+
     params = pd.DataFrame(parameters, columns=names)
     params.to_csv(PARAMETER_PATH)
 
+
 def read_parameters():
-    
+
     return list(pd.read_csv(PARAMETER_PATH, index_col=0).values)
