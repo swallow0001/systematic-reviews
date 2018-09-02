a45f8b9e5b803c46b4af5f6275356b38f051d78b
Jonathan de Bruin <jonathandebruinhome@gmail.com>
2018-09-02 14:21:28 +0200

    YAPF and minor changes to code

--- examples/ptsd_active_deep.py
+++ examples/ptsd_active_deep.py
@@ -12,23 +12,15 @@ performance of using UncertaintySampling and RandomSampling under
 LogisticRegression.
 """
 
-import copy
+
 import argparse
 
+# external dependencies
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix, recall_score
-
-from model.textmanager import TextManager
-from model.embedding import Embedding_Layer
-from model.lstm import LSTM_Model
-
-# keras
-# from keras.preprocessing.text import Tokenizer
-# from keras.preprocessing.sequence import pad_sequences
-# from keras.utils import to_categorical
 
 # libact classes
 from libact.base.dataset import Dataset
@@ -36,53 +28,74 @@ from libact.base.dataset import Dataset
 from libact.query_strategies import UncertaintySampling, RandomSampling
 from libact.labelers import InteractiveLabeler, IdealLabeler
 
-# demo utils
+# project dependencies
+from python.model.textmanager import TextManager
+from python.model.embedding import Word2VecEmbedding
+from python.model.lstm import LSTM_Model
+from python.config import LOG_DIR, OUTPUT_DIR, DATA_DIR, GLOVE_DIR
+from python.libact.labeler import InteractivePaperLabeler
 from python.utils import load_ptsd_data, load_drug_data, word2vec_filepath
-from labeler import InteractivePaperLabeler
 
+# keras
+# from keras.preprocessing.text import Tokenizer
+# from keras.preprocessing.sequence import pad_sequences
+# from keras.utils import to_categorical
+
+# demo utils
 
 # parse arguments if available
 parser = argparse.ArgumentParser(description='Active learning parameters')
 
 parser.add_argument(
-    "--dataset", type=str, default='ptsd', help="The dataset to use for training.")
+    "--dataset",
+    type=str,
+    default='ptsd',
+    help="The dataset to use for training.")
 
 # the number of iterations
 parser.add_argument(
     "--quota", type=int, default=10, help="The number of queries")
 
 # interactive or not
-parser.add_argument('--interactive', dest='interactive', action='store_true',
-                    help="Interactive or not?")
-parser.add_argument('--no-interactive', dest='interactive',
-                    action='store_false')
+parser.add_argument(
+    '--interactive',
+    dest='interactive',
+    action='store_true',
+    help="Interactive or not?")
+parser.add_argument(
+    '--no-interactive', dest='interactive', action='store_false')
 parser.set_defaults(interactive=False)
 
 # type of model
-parser.add_argument('--model', type=str,
-                    default='LSTM',
-                    help="A deep learning model to use for classification.")
+parser.add_argument(
+    '--model',
+    type=str,
+    default='LSTM',
+    help="A deep learning model to use for classification.")
 
 
 def get_indices_labeled_entries(dataset):
     """Get labeled indices"""
 
-    return [idx for idx, entry in enumerate(dataset.data) if entry[1] is not None]
+    return [
+        idx for idx, entry in enumerate(dataset.data) if entry[1] is not None
+    ]
 
 
 # def tranform_text_data(texts):
-# 
+#
 #     # tokenizer
 #     tokenizer = Tokenizer()
 #     tokenizer.fit_on_texts(texts)
-# 
+#
 #     sequences = tokenizer.texts_to_sequences(texts)
 #     data = pad_sequences(sequences,
 #                          maxlen=maxlen,
 #                          padding='post', truncating='post')
-# 
+#
 #     return data, tokenizer.word_index
-# 
+#
+
 
 def make_pool(X, y, prelabeled=np.arange(5)):
     """Function to split dataset into train and test dataset.
@@ -98,7 +111,7 @@ def make_pool(X, y, prelabeled=np.arange(5)):
     print(y.shape)
     # a set of labels is already labeled by the oracle
     # y_train_labeled = np.array([None] * len(y))
-    y_train_labeled =np.empty((len(y), 2))* np.nan
+    y_train_labeled = np.empty((len(y), 2)) * np.nan
     y_train_labeled[prelabeled] = y[prelabeled]
 
     # we are making a pool of the train data
@@ -110,110 +123,109 @@ def main(args):
 
     acc_pool = []
     maxlen = 100
-    
+
     if args.dataset == 'ptsd':
         texts, labels = load_ptsd_data()
-    else: 
+    else:
         texts, labels = load_drug_data(args.dataset)
 
     # get the texts and their corresponding labels
     textmanager = TextManager()
-    data, labels,word_index = textmanager.sequence_maker(texts,labels)
+    data, labels, word_index = textmanager.sequence_maker(texts, labels)
     max_num_words = textmanager.max_num_words
     max_sequence_length = textmanager.max_sequence_length
 
-    embedding = Embedding_Layer(word_index,max_num_words,max_sequence_length)
+    embedding = Word2VecEmbedding(word_index, max_num_words,
+                                  max_sequence_length)
     embedding.load_word2vec_data(word2vec_filepath)
     embedding_layer = embedding.build_embedding()
 
-        
-   
-#     from sklearn.feature_extraction.text import CountVectorizer
-#     from sklearn.feature_extraction.text import TfidfTransformer
-#     from libact.models import SklearnProbaAdapter, SklearnAdapter
-# 
-#     from sklearn.naive_bayes import MultinomialNB
-#     from sklearn.svm import SVC
-#     from sklearn.linear_model import LogisticRegression
+    #     from sklearn.feature_extraction.text import CountVectorizer
+    #     from sklearn.feature_extraction.text import TfidfTransformer
+    #     from libact.models import SklearnProbaAdapter, SklearnAdapter
+    #
+    #     from sklearn.naive_bayes import MultinomialNB
+    #     from sklearn.svm import SVC
+    #     from sklearn.linear_model import LogisticRegression
 
     # # count words
     # count_vect = CountVectorizer(max_features=5000, stop_words='english')
     # features = count_vect.fit_transform(texts).todense().tolist()
 
-        
     pool, pool_ideal = make_pool(
-        data, labels,
-        prelabeled=[1, 2, 3, 4, 5, 218, 260, 466, 532, 564]
-    )
+        data, labels, prelabeled=[1, 2, 3, 4, 5, 218, 260, 466, 532, 564])
 
     # get the model
     if args.model.lower() == 'lstm':
         deep_model = LSTM_Model
         kwargs_model = {
-        'backwards':False,
-        'dropout':0,
-        'optimizer':'rmsprop',
-        'max_sequence_length':max_sequence_length,
-        'embedding_layer':embedding_layer
+            'backwards': False,
+            'dropout': 0,
+            'optimizer': 'rmsprop',
+            'max_sequence_length': max_sequence_length,
+            'embedding_layer': embedding_layer
         }
     elif args.model.lower() == 'cnn':
         deep_model = LSTM_Model
-        kwargs_model = {
-        }
+        kwargs_model = {}
     else:
         raise ValueError('Model not found.')
-# 
+#
 #     # initialize the model through the adapter
 #     model = SklearnProbaAdapter(sklearn_model(**kwargs_model))
     model = deep_model(**kwargs_model)
 
     print(model)
-        
-#     # query strategy
-#     # https://libact.readthedocs.io/en/latest/libact.query_strategies.html
-#     # #libact-query-strategies-uncertainty-sampling-module
-#     #
-#     # least confidence (lc), it queries the instance whose posterior
-#     # probability of being positive is nearest 0.5 (for binary
-#     # classification); smallest margin (sm), it queries the instance whose
-#     # posterior probability gap between the most and the second probable
-#     # labels is minimal
-#     qs = UncertaintySampling(
-#         pool, method='lc', model=SklearnProbaAdapter(sklearn_model(**kwargs_model)))
 
-    qs = UncertaintySampling(
-        pool, method='lc', model=model)
- 
-#     # The passive learning model. The model given in the query strategy is not
-#     # the same. Have a look at this one.
-#     # model = LogisticRegression()
-# 
+    #     # query strategy
+    #     # https://libact.readthedocs.io/en/latest/libact.query_strategies.html
+    #     # #libact-query-strategies-uncertainty-sampling-module
+    #     #
+    #     # least confidence (lc), it queries the instance whose posterior
+    #     # probability of being positive is nearest 0.5 (for binary
+    #     # classification); smallest margin (sm), it queries the instance whose
+    #     # posterior probability gap between the most and the second probable
+    #     # labels is minimal
+    #     qs = UncertaintySampling(
+    #         pool, method='lc', model=SklearnProbaAdapter(sklearn_model(**kwargs_model)))
+
+    qs = UncertaintySampling(pool, method='lc', model=model)
+
+    #     # The passive learning model. The model given in the query strategy is not
+    #     # the same. Have a look at this one.
+    #     # model = LogisticRegression()
+    #
     fig, ax = plt.subplots()
     ax.set_xlabel('Number of Queries')
     ax.set_ylabel('Value')
 
     # Train the model on the train dataset.
     model.train(pool)
-# 
+    #
     # the accuracy of the entire pool
     acc_pool = np.append(
-        acc_pool,
-        model._model.score([x[0] for x in pool.get_entries()], labels)
-    )
-# 
-#     # make plot
+        acc_pool, model._model.score([x[0] for x in pool.get_entries()],
+                                     labels))
+    #
+    #     # make plot
     query_num = np.arange(0, 1)
     p2, = ax.plot(query_num, acc_pool, 'r', label='Accuracy')
-    plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.05), fancybox=True,
-                shadow=True, ncol=5)
+    plt.legend(
+        loc='upper center',
+        bbox_to_anchor=(0.5, -0.05),
+        fancybox=True,
+        shadow=True,
+        ncol=5)
     plt.show(block=False)
-# 
+    #
     # Give each label its name (labels are from 0 to n_classes-1)
     if args.interactive:
         lbr = InteractivePaperLabeler(label_name=["0", "1"])
     else:
         lbr = IdealLabeler(dataset=pool_ideal)
-# 
+
+
+#
     query_i = 1
 
     while query_i <= args.quota:
@@ -223,8 +235,8 @@ def main(args):
         ask_id = qs.make_query()
         print("Index {} returned. True label is {}.".format(
             ask_id, pool_ideal.data[ask_id][1]))
-# 
-#         # get the paper
+        #
+        #         # get the paper
         data_point = pool.data[ask_id][0]
         lb = lbr.label(data_point)
 
@@ -234,12 +246,11 @@ def main(args):
         # train the model again
         model.train(pool)
 
-#         # append the score to the model
+        #         # append the score to the model
         acc_pool = np.append(
             acc_pool,
-            model._model.score([x[0] for x in pool.get_entries()], labels)
-         )
-# 
+            model._model.score([x[0] for x in pool.get_entries()], labels))
+        #
         # additional evaluations
         pred = model.predict([x[0] for x in pool.get_entries()])
         print(confusion_matrix(labels, pred))
