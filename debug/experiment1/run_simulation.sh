3d2bff0ffc6b3c75c2fdbdaff02485fbfba1eb8e
Jonathan de Bruin <jonathandebruinos@gmail.com>
2018-10-29 12:14:36 +0100

    First debug experiment

---
+++ debug/experiment1/run_simulation.sh
@@ -0,0 +1,13 @@
+python hpc/data_prep.py --dataset=ptsd
+
+python3 debug/experiment1/sr_lstm_active.py -T=0 --init_included_papers=15 --batch_size=500 --query_strategy=random --quota=1
+python3 debug/experiment1/sr_lstm_active.py -T=1 --init_included_papers=15 --batch_size=500 --query_strategy=random --quota=1
+python3 debug/experiment1/sr_lstm_active.py -T=2 --init_included_papers=15 --batch_size=500 --query_strategy=random --quota=1
+python3 debug/experiment1/sr_lstm_active.py -T=3 --init_included_papers=15 --batch_size=500 --query_strategy=random --quota=1
+python3 debug/experiment1/sr_lstm_active.py -T=4 --init_included_papers=15 --batch_size=500 --query_strategy=random --quota=1
+python3 debug/experiment1/sr_lstm_active.py -T=5 --init_included_papers=15 --batch_size=500 --query_strategy=random --quota=1
+python3 debug/experiment1/sr_lstm_active.py -T=6 --init_included_papers=15 --batch_size=500 --query_strategy=random --quota=1
+python3 debug/experiment1/sr_lstm_active.py -T=7 --init_included_papers=15 --batch_size=500 --query_strategy=random --quota=1
+python3 debug/experiment1/sr_lstm_active.py -T=8 --init_included_papers=15 --batch_size=500 --query_strategy=random --quota=1
+python3 debug/experiment1/sr_lstm_active.py -T=9 --init_included_papers=15 --batch_size=500 --query_strategy=random --quota=1
+python3 debug/experiment1/sr_lstm_active.py -T=10 --init_included_papers=15 --batch_size=500 --query_strategy=random --quota=1
---
+++ debug/experiment1/sr_lstm_active.py
@@ -0,0 +1,243 @@
+#!/usr/bin/env python3
+"""
+This script simulates real world use of active learning algorithms. Which in the
+start, there are only a small fraction of samples are labeled. During active
+learing process active learning algorithm (QueryStrategy) will choose a sample
+from unlabeled samples to ask the oracle to give this sample a label (Labeler).
+
+In this example, ther dataset are from the digits dataset from sklearn. User
+would have to label each sample choosed by QueryStrategy by hand. Human would
+label each selected sample through InteractiveLabeler. Then we will compare the
+performance of using UncertaintySampling and RandomSampling under
+LogisticRegression.
+"""
+
+import os
+import copy
+import argparse
+import sys
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix, recall_score
+
+# project dependencies
+sys.path.insert(0, os.path.join('src', 'python'))  # path to the module.
+
+from models.lstm_libact import LSTM_Libact
+from query_strategies.uncertainty_sampling import UncertaintySampling
+from query_strategies.random_sampling import RandomSampling
+
+from sklearn.model_selection import StratifiedKFold
+
+# libact classes
+from libact.base.dataset import Dataset
+from libact.labelers import InteractiveLabeler, IdealLabeler
+from libact_utils.labeler import InteractivePaperLabeler
+
+# demo utils
+from utils import *
+from config import *
+
+# parse arguments if available
+parser = argparse.ArgumentParser(description='Active learning parameters')
+parser.add_argument("-T", default=1, type=int, help='Task number.')
+parser.add_argument(
+    "--dataset",
+    type=str,
+    default='ptsd',
+    help="The dataset to use for training.")
+
+# the number of iteration
+parser.add_argument(
+    "--quota", type=int, default=10, help="The number of queries")
+parser.add_argument(
+    '--interactive',
+    dest='interactive',
+    action='store_true',
+    help="Interactive or not?")
+parser.add_argument(
+    '--no-interactive', dest='interactive', action='store_false')
+parser.set_defaults(interactive=False)
+
+parser.add_argument(
+    "--init_included_papers",
+    default=10,
+    type=int,
+    help='Initial number of included papers')
+
+parser.add_argument(
+    "--batch_size",
+    default=10,
+    type=int,
+    help='Batch size')
+
+
+parser.add_argument(
+    '--model',
+    type=str,
+    default='LSTM',
+    help="A deep learning model to use for classification.")
+parser.add_argument(
+    "--query_strategy", type=str, default='lc', help="The query strategy")
+
+
+def get_indices_labeled_entries(dataset):
+    """Get labeled indices"""
+    return [
+        idx for idx, entry in enumerate(dataset.data) if entry[1] is not None
+    ]
+
+
+def select_prelabeled(labels, init_included, seed):
+    """Draw sample of records to initialize the pool."""
+    included_indexes = np.where(labels[:, 1] == 1)[0]
+    excluded_indexes = np.where(labels[:, 1] == 0)[0]
+    included_number = min(init_included, len(included_indexes))
+    np.random.seed(seed)
+    to_add_indx = np.random.choice(
+        included_indexes, included_number, replace=False)
+    to_add_indx = np.append(
+        to_add_indx,
+        np.random.choice(excluded_indexes, included_number, replace=False))
+    return to_add_indx
+
+
+def make_pool(X, y, prelabeled=np.arange(5)):
+    """Function to split dataset into train and test dataset.
+
+    Arguments
+    ------
+
+    prelabeled: list
+        List of indices for which the label is already available.
+
+    """
+    y = y.argmax(axis=1)
+    # a set of labels is already labeled by the oracle
+    y_train_labeled = np.array([None] * len(y))
+    #y_train_labeled =np.empty((len(y), 2))* np.nan
+    y_train_labeled[prelabeled] = y[prelabeled]
+
+    # we are making a pool of the train data
+    # the 'prelabeled' labels of the dataset are already labeled.
+    return Dataset(X, y_train_labeled), Dataset(X, y)
+
+
+def main(args):
+
+    # Read dataset, labels and embedding layer from pickle file.
+    pickle_fp = os.path.join(TEMP_DATA_DIR, args.dataset + '_pickle.pickle')
+    with open(pickle_fp, 'rb') as f:
+        data, labels, embedding_layer = pickle.load(f)
+
+    # label the first batch (the initial labels)
+    seed = 2018 * args.T
+    prelabeled_index = select_prelabeled(labels, args.init_included_papers,
+                                         seed)
+    # [1, 2, 3, 4, 5, 218, 260, 466, 532, 564]
+    print('prelabeled_index', prelabeled_index)
+
+    pool, pool_ideal = make_pool(data, labels, prelabeled=prelabeled_index)
+
+    # get the model
+    if args.model.lower() == 'lstm':
+        deep_model = LSTM_Libact
+        kwargs_model = {
+            'backwards': True,
+            'dropout': 0.4,
+            'optimizer': 'rmsprop',
+            'max_sequence_length': 1000,
+            'embedding_layer': embedding_layer
+        }
+    else:
+        raise ValueError('Model not found.')
+
+    model = deep_model(**kwargs_model)
+    # store the starting memory of the model
+    init_weights = model._model.get_weights()
+
+    # Give each label its name (labels are from 0 to n_classes-1)
+    if args.interactive:
+        lbr = InteractivePaperLabeler(label_name=["0", "1"])
+    else:
+        lbr = IdealLabeler(dataset=pool_ideal)
+
+    result_df = pd.DataFrame({'label': [x[1] for x in pool_ideal.data]})
+    query_i = 0
+
+    while query_i < args.quota:
+
+        # make a query from the pool
+        print("Asking sample from pool (qs %s)" % args.query_strategy)
+        # unlabeled_entry = pool.get_unlabeled_entries()
+
+        # train the model
+        model.train(pool)
+
+        # predict the label of the unlabeled entries in the pool
+        idx_features = pool.get_unlabeled_entries()
+        idx = [x[0] for x in idx_features]
+        features = [x[1] for x in idx_features]
+        pred = model.predict(features)
+
+        # make query
+        if (args.query_strategy =='lc'):
+            qs = UncertaintySampling(
+            pool, method='lc', model=model)
+        elif (args.query_strategy =='random'):
+            qs = RandomSampling(pool)
+
+        ask_id = qs.make_query(n=args.batch_size)
+
+        if not isinstance(ask_id, list):
+            ask_id = [ask_id]
+
+        # deal with batch query
+        for id in ask_id:
+
+            # label the entry
+            data_point = pool.data[id][0]
+            lb = lbr.label(data_point)
+            print("Index {} returned. True label is {}.".format(
+                id, lb))
+
+            # update the pool with the new label
+            pool.update(id, lb)
+
+        # store result in dataframe
+        c_name = str(query_i)
+        result_df[c_name] = -1
+        result_df.loc[idx, c_name] = pred[:, 1]
+
+        # reset the weights
+        model._model.set_weights(init_weights)
+
+        # update the query counter
+        query_i += 1
+
+    # save the result to a file
+    output_dir = os.path.join(ACTIVE_OUTPUT_DIR, args.dataset)
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    export_path = os.path.join(
+        output_dir, 'dataset_{}_sr_lstm_active{}_q_{}.csv'.format(
+            args.dataset, args.T, args.query_strategy))
+
+    result_df.to_csv(export_path)
+    input("Press any key to continue...")
+
+
+if __name__ == '__main__':
+
+    # parse all the arguments
+    args = parser.parse_args()
+    print("input args: %s" % args)
+
+    try:
+        # start the active learning algorithm
+        main(args)
+    except KeyboardInterrupt:
+        print('Closing down.')
